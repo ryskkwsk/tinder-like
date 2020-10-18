05e0ab515fffc4133e37d570d29fc3e1ab6024bd
RyosukeKawasaki <kawasakiryosuke@team-lab.com>
2020-10-18 14:06:54 +0900

    [add]編集機能追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use App\Services\CheckExtensionServices;
+use App\Services\FileUploadServices;
 
 use App\User;
 
@@ -13,5 +16,41 @@ class UserController extends Controller
         $user = User::findorFail($id);
 
         return view('users.show', compact('user'));
+    }
+
+    public function edit($id)
+    {
+        $user = User::findorFail($id);
+
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findorFail($id);
+
+        if(!is_null($request['img_name']))
+        {
+            $imageFile = $request['img_name'];
+
+            $list = FileUploadServices::fileUpload($imageFile);
+
+            list($extension, $fileNameToStore, $fileData) = $list;
+
+            $data_url = CheckExtensionServices::checkExtension($fileData, $extension);
+            $image = Image::make($data_url);
+            $image->resize(400,400)->save(storage_path() . '/app/public/images/' . $fileNameToStore );
+
+            $user->img_name = $fileNameToStore;
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->sex = $request->sex;
+        $user->self_introduction = $request->self_introduction;
+
+        $user->save();
+
+        return redirect('home');
     }
 }
